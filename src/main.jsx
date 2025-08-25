50e672dc1e481cb12b3c7ea45842f9eb2250018e
santosh-006 <santoshkumark2005@gmail.com>
2025-08-25 23:01:33 +0530

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App/>
-)
+  <React.StrictMode>
+    <BrowserRouter basename="/Tic-Tac-Toe/">
+    <Routes>
+      <Route path="*" element = {<App/>}/>
+    </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
